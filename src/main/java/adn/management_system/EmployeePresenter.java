ac713f78b260957bddbe29e68532f451b6b5bf65
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 14:00:42 -0500

    Created the constructor for the database

--- src/main/java/adn/management_system/EmployeePresenter.java
+++ src/main/java/adn/management_system/EmployeePresenter.java
@@ -4,6 +4,11 @@ public class EmployeePresenter implements IMVPContract.Presenter {
     EmployeeDB_Mem employeeDB;
     IMVPContract.View theActualView;
 
+    public EmployeePresenter(IMVPContract.View aView) {
+        this.theActualView = aView;
+        this.employeeDB = new EmployeeDB_Mem();
+    }
+
     @Override
     public void addEmployeeToDB(String initFirstName, String initLastName, String initSalary, String initJobTitle, String initYearsAtCompany) {
         Integer eYears = Integer.parseInt( initYearsAtCompany );
