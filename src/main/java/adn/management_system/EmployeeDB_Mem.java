f97b2cabd0ac01a8818cc89592cac3bc1587bfcd
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 12:36:20 -0500

    Added the constructor for the employee database

--- src/main/java/adn/management_system/EmployeeDB_Mem.java
+++ src/main/java/adn/management_system/EmployeeDB_Mem.java
@@ -1,6 +1,14 @@
 package adn.management_system;
 
+import java.util.LinkedList;
+
 public class EmployeeDB_Mem implements IEmployeeDB{
+
+    private final LinkedList<Employee> db;
+
+    public EmployeeDB_Mem() {
+        this.db = new LinkedList<>();
+    }
 
     @Override
     public void addEmployee(Employee e) {
