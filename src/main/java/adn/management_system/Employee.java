00d41900f28a08180e11abd6a631188e274de566
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 10:49:32 -0500

    Created the Object Creation and Variables for the Employee Class

--- src/main/java/adn/management_system/Employee.java
+++ src/main/java/adn/management_system/Employee.java
@@ -1,4 +1,25 @@
 package adn.management_system;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private String jobTitle;
+    private int yearsAtCompany;
+
+    private Employee() {
+        this.firstName = "Unknown";
+        this.lastName = "Unknown";
+        this.salary = 0;
+        this.jobTitle = "Unknown";
+        this.yearsAtCompany = 0;
+    }
+
+    Employee(String initFirstName, String initLastName, int initSalary, String initJobTitle, int initYearsAtCompany) {
+        this.firstName = initFirstName;
+        this.lastName = initLastName;
+        this.salary = initSalary;
+        this.jobTitle = initJobTitle;
+        this.yearsAtCompany = initYearsAtCompany;
+    }
 }
