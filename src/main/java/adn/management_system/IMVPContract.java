9702b2b69518cf07a4e1cfdb877487a23d8e40be
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 13:20:16 -0500

    Added the interfaces and the methods

--- src/main/java/adn/management_system/IMVPContract.java
+++ src/main/java/adn/management_system/IMVPContract.java
@@ -1,4 +1,12 @@
 package adn.management_system;
 
 public class IMVPContract {
+
+    interface Presenter {
+        void addEmployeeToDB( String initFirstName, String initLastName, int initSalary, String initJobTitle, int initYearsAtCompany );
+    }
+
+    interface View {
+        void updateNumberInDB( int num );
+    }
 }
