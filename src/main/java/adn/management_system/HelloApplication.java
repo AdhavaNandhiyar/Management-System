32f54caf95aee16f081c0e59a981ecfd418e6889
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 14:11:37 -0500

    Added the textfields and presenter and methods for the mvp contract

--- src/main/java/adn/management_system/HelloApplication.java
+++ src/main/java/adn/management_system/HelloApplication.java
@@ -2,19 +2,44 @@ package adn.management_system;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements IMVPContract.View {
+
+    private TextField tf_firstNameField;
+    private TextField tf_lastNameField;
+    private TextField tf_salaryField;
+    private TextField tf_jobField;
+    private TextField tf_yearsField;
+
+    IMVPContract.Presenter myPresenter;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+
+
+
+
+
+
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void updateNumberInDB(int num) {
+
     }
 
     public static void main(String[] args) {
