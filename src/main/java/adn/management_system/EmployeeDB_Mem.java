59be23841578eda698ffa6ef199c943acd5b7b4a
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 12:34:58 -0500

    Added Methods from the employee database interface

--- src/main/java/adn/management_system/EmployeeDB_Mem.java
+++ src/main/java/adn/management_system/EmployeeDB_Mem.java
@@ -1,4 +1,14 @@
 package adn.management_system;
 
-public class EmployeeDB_Mem {
+public class EmployeeDB_Mem implements IEmployeeDB{
+
+    @Override
+    public void addEmployee(Employee e) {
+
+    }
+
+    @Override
+    public int numInDB() {
+        return 0;
+    }
 }
