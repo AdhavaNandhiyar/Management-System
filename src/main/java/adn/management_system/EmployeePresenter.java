4a54a18cbfc964489d15504c2e68797400da4b14
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-28 13:58:33 -0500

    Added and Created the method to add an employee to the database

--- src/main/java/adn/management_system/EmployeePresenter.java
+++ src/main/java/adn/management_system/EmployeePresenter.java
@@ -1,4 +1,17 @@
 package adn.management_system;
 
-public class EmployeePresenter implements IMVPContract.Presenter{
+public class EmployeePresenter implements IMVPContract.Presenter {
+    EmployeeDB_Mem employeeDB;
+    IMVPContract.View theActualView;
+
+    @Override
+    public void addEmployeeToDB(String initFirstName, String initLastName, String initSalary, String initJobTitle, String initYearsAtCompany) {
+        Integer eYears = Integer.parseInt( initYearsAtCompany );
+        Integer eSalary = Integer.parseInt( initSalary );
+
+        Employee e = new Employee(initFirstName, initLastName, eSalary, initJobTitle, eYears);
+        employeeDB.addEmployee(e);
+
+        theActualView.updateNumberInDB( employeeDB.numInDB() );
+    }
 }
