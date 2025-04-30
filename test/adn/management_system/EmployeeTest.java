61205127e128167206f32af8b923683ef383e63d
Adhava805 <adhavasnandhiyar@gmail.com>
2025-04-30 14:41:23 -0500

    Created the first employee creation for the employee test

--- test/adn/management_system/EmployeeTest.java
+++ test/adn/management_system/EmployeeTest.java
@@ -1,7 +1,20 @@
 package adn.management_system;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+    @Test
+    public void testEmployeeCreation() {
+        Employee e = new Employee("Adhava", "Nandhiyar", 10000, "Intern", 2, "0001");
+
+        assertEquals("Adhava", e.getFirstName());
+        assertEquals("Nandhiyar", e.getLastName());
+        assertEquals(10000, e.getSalary());
+        assertEquals("Intern", e.getJobTitle());
+        assertEquals(2, e.getYearsAtCompany());
+        assertEquals("0001", e.getEmployeeID());
+    }
 
 }
