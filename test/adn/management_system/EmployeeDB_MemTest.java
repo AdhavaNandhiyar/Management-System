7f826230022dda870c0ce557a4df5e36f05914e6
Adhava805 <adhavasnandhiyar@gmail.com>
2025-05-01 13:53:56 -0500

    Added JavaDoc to Tests

--- test/adn/management_system/EmployeeDB_MemTest.java
+++ test/adn/management_system/EmployeeDB_MemTest.java
@@ -4,15 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The EmployeeDB_MemTest class tests the various methods and
+ * creations of EmployeeDB_Mem class
+ *
+ * @author Adhava Nandhiyar
+ */
 class EmployeeDB_MemTest {
+
+    /**
+     * This function tests the EmployeeDB_Mem ability to
+     * be created correctly and also if the
+     * functions correctly output
+     */
     @Test
     public void testEmployeeDBCreation() {
         EmployeeDB_Mem db = new EmployeeDB_Mem();
         Employee e = new Employee("Adhava", "Nandhiyar", 10000, "Intern", 2, "0001");
 
         db.addEmployee(e);
-
         assertEquals(1, db.numInDB());
+
+        assertEquals(e, db.findEmployee("0001"));
+
+        db.removeEmployeeFromDB(e);
+        assertEquals(0, db.numInDB());
     }
 
 }
--- test/adn/management_system/EmployeeTest.java
+++ test/adn/management_system/EmployeeTest.java
@@ -4,7 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The EmployeeTest class tests the various methods and
+ * creations of employee class
+ *
+ * @author Adhava Nandhiyar
+ */
 class EmployeeTest {
+
+    /**
+     * This function tests the employee ability to
+     * be created correctly
+     */
     @Test
     public void testEmployeeCreation() {
         Employee e = new Employee("Adhava", "Nandhiyar", 10000, "Intern", 2, "0001");
